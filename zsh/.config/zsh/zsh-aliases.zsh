2889ba6af6fab275135b4542d6fdce4a5846b8c4
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2023-02-27 14:23:40 -1000

    remove -h from exa

--- zsh/.config/zsh/zsh-aliases.zsh
+++ zsh/.config/zsh/zsh-aliases.zsh
@@ -22,10 +22,10 @@ alias cl=clear
 
 # ls using exa
 alias ls="exa --icons --color=auto --group --git"               
-alias l="exa -lah --time-style long-iso --icons --color=auto --group --git"        
-alias ll="exa -lh --time-style long-iso --icons --color=auto --group --git"
+alias l="exa -la --time-style long-iso --icons --color=auto --group --git"        
+alias ll="exa -l --time-style long-iso --icons --color=auto --group --git"
 alias lll="exa -lasnew --time-style long-iso --icons --color=auto --group --git"        
-alias lt='exa -Tlah --time-style long-iso --icons --color=auto --group --git'
+alias lt='exa -Tla --time-style long-iso --icons --color=auto --group --git'
 
 # CD Shortcuts
 alias docs="cd ~/Documents"
