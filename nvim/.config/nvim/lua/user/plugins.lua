2f069c6d456fbcef42e61e75c83993ba811db3dc
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2023-03-18 18:07:55 -1000

    updated plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -114,7 +114,6 @@ return require('packer').startup(function(use)
   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 }
 
-
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
