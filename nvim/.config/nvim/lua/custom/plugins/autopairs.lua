12ed2d3a8cacb7aec257d743a77d506093b9595f
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2023-09-26 09:29:35 -1000

    added nvim plugin configuration

---
+++ nvim/.config/nvim/lua/custom/plugins/autopairs.lua
@@ -0,0 +1,40 @@
+-- File: lua/custom/plugins/autopairs.lua
+
+return {
+  "windwp/nvim-autopairs",
+  -- Optional dependency
+  dependencies = { 'hrsh7th/nvim-cmp' },
+  config = function()
+    require("nvim-autopairs").setup {}
+    -- If you want to automatically add `(` after selecting a function or method
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
+
+    local npairs = require("nvim-autopairs")
+    local Rule = require('nvim-autopairs.rule')
+    -- put this to setup function and press <a-e> to use fast_wrap
+    npairs.setup({
+      fast_wrap = {},
+    })
+    -- change default fast_wrap
+    npairs.setup({
+      fast_wrap = {
+        map = '<M-e>',
+        chars = { '{', '[', '(', '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = '$',
+        before_key = 'h',
+        after_key = 'l',
+        cursor_pos_before = true,
+        keys = 'qwertyuiopzxcvbnmasdfghjkl',
+        manual_position = true,
+        highlight = 'Search',
+        highlight_grey = 'Comment'
+      },
+    })
+  end,
+}
