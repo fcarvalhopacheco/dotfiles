0faeb66e14fc1f18ae3a95ed92ce2392fbe3f793
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2023-10-30 20:43:32 -1000

    started telekasten

---
+++ nvim/.config/nvim/lua/custom/plugins/telekasten.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'renerocksai/telekasten.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'renerocksai/calendar-vim'
+    },
+    config = function()
+      require('telekasten').setup({
+        home = vim.fn.expand("~/Documents/notes"), -- Put the name of your notes directory here
+
+      })
+    end,
+  },
+
+}
